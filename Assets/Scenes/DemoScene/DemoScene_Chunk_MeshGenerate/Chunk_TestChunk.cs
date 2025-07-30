d8ee0fc8507d579b54761253c0304f7069f3805d
MuaCherish <317426168@qq.com>
2025-07-30 18:02:42 +0800

    V[Alpha-9] —— 开发DemoScene_Chunk_MeshGenerate [Edit] 1.
    [Add]
    1. 添加项目一键预设器

---
+++ Assets/Scenes/DemoScene/DemoScene_Chunk_MeshGenerate/Chunk_TestChunk.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DemoScene_Chunk_MeshGenerate
+{
+    public class Chunk_TestChunk
+    {
+        
+    }
+
+}
